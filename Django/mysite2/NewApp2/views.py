d910b3c79982aedcd19e40d3d4fa04cce99ad091
Woneata Stallworth <woneata@gmail.com>
2021-05-12 17:58:19 +0300

    created a new app

--- Django/mysite2/NewApp2/views.py
+++ Django/mysite2/NewApp2/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 data = {
-    
+
     "Name: Woneata Stallworth ",
     "Track: Backend(Python) ",
     "Message: Hi, mentor. Fingercrossed I don't have to redo this lol ",
