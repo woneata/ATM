d262d544dd3bee54c51c51289f524e60104b1b63
Woneata Stallworth <woneata@gmail.com>
2021-05-12 17:53:49 +0300

    commit

---
+++ Django/mysite2/NewApp2/views.py
@@ -0,0 +1,16 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+data = {
+    
+    "Name: Woneata Stallworth ",
+    "Track: Backend(Python) ",
+    "Message: Hi, mentor. Fingercrossed I don't have to redo this lol ",
+    
+
+}
+
+def index(request):
+    return HttpResponse(data)
